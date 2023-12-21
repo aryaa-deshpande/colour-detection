06169bf85d1af8d0e4aed23a650836e854d4c13c
aryaa-deshpande <140228656+aryaa-deshpande@users.noreply.github.com>
2023-12-21 11:15:36 +0530

    Add files via upload

---
+++ all_code.py
@@ -0,0 +1,59 @@
+# MAIN CODE
+
+import cv2
+import torch
+import datetime
+
+
+# yolov5 model for object detection
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# path to the sample video 
+video_path = "input2.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# output folder where the captured frames will be saved 
+output_folder = 'C:\\Users\\aryaa\\OneDrive\\Desktop\\VSCode\\projects\\traffic_violation_detection\\cars2'
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # applying the model for object detection to the video frame by frame 
+    results = model(frame)
+    cv2.rectangle(frame,(642,151), (1274,220), (0,255,255),2) #for the zebra crossing 
+
+    for detection in results.pred[0]:
+        class_id, conf, bbox = detection[5], detection[4], detection[:4]
+
+        # class 2 represents cars 
+        if int(class_id) == 2 or int(class_id) == 3 or int(class_id) == 7: 
+
+            # finding coordinates of all the cars detected 
+            x, y, w, h = map(int, bbox)
+            center_x = int(x + w / 2)
+            center_y = int(y + h / 2)
+            center_coordinates = (center_x,center_y)
+                        
+            # drawing bounding boxes for cars crossing the zebra crossing 
+            # corrdinates of the zebra crossing can be edited depending on the video
+            if (center_coordinates[0] in range(1500,1584) or center_coordinates[0] in range(800,1400)) :
+                cv2.rectangle(frame, (x, y), (w, h), (255, 255, 0), 2)
+                                
+                timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+                output_path = f"{output_folder}/{timestamp}.jpg"
+
+                # saving just the cars around which bounding boxes are drawn
+                cv2.imwrite(output_path, frame[y:h,x:w])
+
+                       
+    cv2.imshow("Car Detection", frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release() 
+cv2.destroyAllWindows()
+
+
---
+++ ocr.py
@@ -0,0 +1,92 @@
+import os 
+from paddleocr import PaddleOCR
+import cv2
+import psycopg2
+import datetime
+import time
+import pandas as pd
+from PIL import Image
+import numpy as np
+from collections import Counter
+
+
+# connecting to the database
+connection = psycopg2.connect(user="postgres",
+                              password="Aryaa2002!",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="traffic_violation")
+
+# input video
+input = 'car_plates3'
+numbers = []
+# ocr for detection
+ocr = PaddleOCR(lang='en')
+
+
+# very specific colour dataset 
+index =  ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('Color-Detection-OpenCV-main\colors.csv', names=index, header=None)\
+
+# # alternate  dataset
+# index = [ "R", "G", "B","colour"]
+# csv = pd.read_csv('colour.csv', names=index, header=None)
+
+def color(image):
+  image_array = np.array(image)
+  pixels = image_array.reshape(-1, 3)
+  
+#   counting the number of times each colour combo is seen in the image
+  colour_counts = Counter(tuple(color) for color in pixels)
+
+#   finds the colour which appears the most
+  main_colour = colour_counts.most_common(1)[0][0]
+
+# compares the most appeared colour with the dataset to get the name of colour
+  colour_differences = [
+        abs(main_colour[0] - int(row["R"])) + abs(main_colour[1] - int(row["G"])) + abs(main_colour[2] - int(row["B"]))
+        for _, row in csv.iterrows()
+    ]
+  min_difference = colour_differences.index(min(colour_differences))
+  
+  return csv.loc[min_difference, "color_name"]
+
+
+image_folder = "car_plates3"
+
+
+
+
+
+# applying ocr on captured images and saving to database 
+for filename in os.listdir(input):
+    if filename.endswith((".jpg")):
+        image_path = os.path.join(f'{input}\\{filename}')
+        image = cv2.imread(image_path)
+        result = ocr.ocr(image)
+        dominant_colour = color(image)
+        type_of_car = "nothing"
+        
+        try:
+            number_plate = result[0][0][1][0]
+            
+            with connection.cursor() as inner_cur:
+                
+                inner_cur.execute("INSERT INTO number_plates (license_plate, todays_date, time_of_violation, colour, type_of_car, image_name) values (%s,%s,%s,%s,%s,%s);" , (number_plate,datetime.date.today(),time.strftime("%H:%M:%S", time.localtime()),dominant_colour,type_of_car,image_path))
+                inner_cur.execute("SELECT * FROM number_plates")
+                rows = inner_cur.fetchall()
+                for row in rows :
+                    a = row[0]
+                    numbers.append(a) 
+                    
+                # Deleting duplicates of the number plates
+                if numbers.count(number_plate) > 1:
+                    inner_cur.execute("DELETE FROM number_plates WHERE license_plate = %s AND ctid NOT IN (SELECT min(ctid) FROM number_plates WHERE license_plate = %s);", (number_plate, number_plate))
+            connection.commit()
+        except TypeError :
+            continue
+
+
+ 
+
+            
