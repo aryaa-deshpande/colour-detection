9505c4acd59b6ab6fc720a352e80ca566334d3aa
aryaa-deshpande <140228656+aryaa-deshpande@users.noreply.github.com>
2023-12-21 11:28:28 +0530

    db.sql

---
+++ db.sql
@@ -0,0 +1,12 @@
+# CREATING DATABASE
+
+CREATE TABLE number_plates (
+    license_plate VARCHAR(20) ,
+    todays_date DATE,
+    time_of_violation TIME,
+    colour VARCHAR(50),
+    type_of_car VARCHAR(50),
+    image_name VARCHAR(50)
+    
+);
+
